04892f4ea34641110938ca1b20e34cb8d1d07a9d
Aleena-Naveed <aleenanaveed00@gmail.com>
2020-12-10 11:44:02 +0500

    touchableOpacity and Scrollview

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [getText, setText] = useState('');
+  const [getList, setList] = useState([]);
+
+  const addItem = () =>{
+     setList([
+       ...getList, 
+       {key: Math.random().toString(),
+       data: getText}
+      ]);
+     setText('');
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.innerContainer}>
+        <TextInput
+          style={styles.TextStyle}
+          placeholder="Enter Item"
+          onChangeText={text => setText(text)}
+          value={getText}
+        />
+        <Button 
+          title="ADD"
+          onPress={addItem}
+        />
+      </View>
+      <View style={styles.ScrollView}>
+        <ScrollView>
+          {getList.map((item) => 
+          <TouchableOpacity
+          key={item.key}
+          activeOpacity= {0.5}
+          >
+            <View style={styles.scrollViewItem} >
+              <Text style={styles.ScrollText}>{item.data}</Text>
+            </View>
+          </TouchableOpacity>
+          )}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -17,5 +52,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 70
   },
+  innerContainer: {
+    flexDirection: "row",
+    justifyContent: 'space-between',
+  },
+  TextStyle: {
+    borderWidth: 2,
+    borderRadius: 50,
+    width: "60%",
+    padding: 10
+  },
+  ScrollView: {
+    width: "100%"
+  },
+  scrollViewItem: {
+    width: "80%",
+    backgroundColor: "orange",
+    alignSelf: 'center',
+    padding: 10,
+    margin: 5,
+    borderRadius: 50
+  },
+  ScrollText: {
+    fontSize: 16,
+    fontStyle: 'italic',
+    color: 'white'
+  }
 });
